7a114b3385ace856a3a0d39a2594d8aca8bc5c64
Krisli Dimo <krislidimo@gmail.com>
2019-06-13 18:02:17 -0500

    finished mvp | added roles with access to endpoints | stretch: gave admin access to /users

--- src/main/java/com/lambdaschool/zoos/SeedData.java
+++ src/main/java/com/lambdaschool/zoos/SeedData.java
@@ -29,20 +29,42 @@ public class SeedData implements CommandLineRunner
     {
         Role r1 = new Role("admin");
         Role r2 = new Role("user");
+        Role r3 = new Role("zoodata");
+        Role r4 = new Role("animaldata");
+        Role r5 = new Role("mgr");
+
 
         ArrayList<UserRoles> admins = new ArrayList<>();
         admins.add(new UserRoles(new User(), r1));
-        admins.add(new UserRoles(new User(), r2));
+
 
         ArrayList<UserRoles> users = new ArrayList<>();
         users.add(new UserRoles(new User(), r2));
+        users.add(new UserRoles(new User(), r3));
+        users.add(new UserRoles(new User(), r4));
+        users.add(new UserRoles(new User(), r5));
+
 
         rolerepos.save(r1);
         rolerepos.save(r2);
-        User u1 = new User("barnbarn", "ILuvM4th!", users);
-        User u2 = new User("admin", "password", admins);
+        rolerepos.save(r3);
+        rolerepos.save(r4);
+        rolerepos.save(r5);
+
+
+
+        User u1 = new User("admin", "pass!", users);
+        User u2 = new User("zoodata", "pass", admins);
+        User u3 = new User("animaldata", "pass", admins);
+        User u4 = new User("mgr", "pass", admins);
+        User u5 = new User("user", "pass", admins);
+
 
         userrepos.save(u1);
         userrepos.save(u2);
+        userrepos.save(u3);
+        userrepos.save(u4);
+        userrepos.save(u5);
+
     }
 }
